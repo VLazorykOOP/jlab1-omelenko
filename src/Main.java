c9e0c397c6288749315e0bfb5417c83e4554e9b0
omelenko <melenko.oleksandr@chnu.edu.ua>
2025-09-13 19:44:04 +0300

    завдання 3

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Lab 1 Java");
-        task2();
+        task3();
     }
 
     public static void print(String str) {
@@ -19,7 +19,7 @@ public class Main {
     }
 
     public static double c2(int a, int b) {
-        return (((a*b)-((a+b)*(a-b)))/((b*b*b*b)+(a*a*a)))+(5*b);
+        return ((double) ((a * b) - ((a + b) * (a - b))) /((b*b*b*b)+(a*a*a)))+(5*b);
     }
 
     public static int c3(double a, double b) {
@@ -43,7 +43,6 @@ public class Main {
             print("Розмір масиву не може бути більше 200");
             return;
         }
-        
 
         double[] A = new double[2*n];
 
@@ -72,5 +71,45 @@ public class Main {
         }
 
         print("Сума елементів, які більші за середнє арифметичне перших " + n + " елементів: " + sum);
+    }
+
+    public static void task3() {
+        print("\n\nTask 3\n\n");
+        print("Введіть розмірність матриці (nxn): ");
+        int n = sc.nextInt();
+        if(n > 20) {
+            print("Розмірність матриці не може бути більше 20");
+            return;
+        }
+
+        double[][] A = new double[n][n];
+        double[] B = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                A[i][j] = Math.random();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            double average = 0;
+            for (int j = 0; j < n; j++) {
+                average += A[i][j];
+            }
+            B[i] = average / n;
+        }
+
+        print("\n\nВектор B побудований з середніх арифметичних рядків матриці А: \n\n B = [");
+        for (int i = 0; i < n; i++) {
+            if (i < n-1)
+            {
+                print(B[i]+", ");
+            }
+            else
+            {
+                print(B[i]+"\n]");
+            }
+        }
+
     }
 }
